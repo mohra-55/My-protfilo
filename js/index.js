30e238b59c8adee8839e0ab67864cb25017a8370
mohra-55 <mohra2612@gmail.com>
2026-03-11 02:48:10 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const navbar=document.querySelector('#navbar');
+window.addEventListener('scroll',function () {
+    if(window.scrollY>632){
+        navbar.classList.add('scrolled')
+    }
+    else{
+        navbar.classList.remove('scrolled')
+    }
+    
+})
